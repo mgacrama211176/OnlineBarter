b937fcc6a5947ad7751ac847cf42e2b0d5d43942
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-11-19 23:22:39 +0800

    first commit

---
+++ pages/signup/signup.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const signup = () => {
+  return (
+    <div>
+      <h1>SIGNUP PAGE</h1>
+      <Link href="/">
+        <h2>back to home</h2>
+      </Link>
+    </div>
+  );
+};
+
+export default signup;
