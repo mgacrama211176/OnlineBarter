19e47248a807d9c4dc899dedb876715764e55acb
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-11-28 20:00:45 +0800

    Made few changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export const Poster = async (request, response, next) => {
+  if (request.method === "GET") {
+    response.status(200).json(`POSTED!!`);
+  } else {
+    response.status(400).json(`NOT POSTED!!`);
+  }
+};
