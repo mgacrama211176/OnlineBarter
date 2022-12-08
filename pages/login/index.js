d9dad264b2f5175e368a32ad241ec6957d396a2d
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-12-09 00:50:21 +0800

    Update State

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Head from "next/head";
 import { Be_Vietnam_Pro } from "@next/font/google";
 import Logo from "../../component/logo";
